d4f8709de9f6fba8eb47e1203af5156b92594bdd
Danny Armijos <daniel2667@hotmail.es>
2025-05-15 03:09:21 +0200

    refactor: corregir la implementación del pipe 'capitalize' para manejar correctamente el valor de entrada

--- src/app/pipes/capitalize.pipe.ts
+++ src/app/pipes/capitalize.pipe.ts
@@ -1,12 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'capitalize'
+  name: 'capitalize',
 })
 export class CapitalizePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): string {
+    if (!value) {
+      return '';
+    }
+    return value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
   }
-
 }
